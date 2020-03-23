92e78803b54cf9ef2db5f9f0d050a1d0d5cf5d6b
DariaDRMK <miroshnychenko.d.d@gmail.com>
2020-03-23 18:03:33 +0200

    First commit of Lab 2

---
+++ Lab 2/SimCorp.IMS.Course.Test/SIM card tests/LifecellSimCardTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimCorp.IMS.Course.Test.SIM_card_tests
+{
+    [TestClass]
+    public class LifecellSimCardTest
+    {
+        [TestMethod]
+        public void LifecellSimCardConnectToLocalMobileNetwork()
+        {
+            // Arrange
+            string actualOutput = "";
+            OutputForTesting VariableOutput = new OutputForTesting(actualOutput);
+            LifecellSimCard LifecellSimCard = new LifecellSimCard(VariableOutput);
+
+            string expectedOutput = $"{nameof(LifecellSimCard)} has connected to the local mobile network";
+
+            // Act
+            LifecellSimCard.ConnectToLocalMobileNetwork();
+            actualOutput = VariableOutput.OutputVariable;
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
